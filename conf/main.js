fdc9b5ae0f886aeff33be2f4b33b07fc86c46efc
Andrew Osheroff <andrewosh@gmail.com>
2016-02-06 01:47:31 -0500

    Added descriptions and caching

--- conf/main.js
+++ conf/main.js
@@ -1,6 +1,8 @@
 var path = require('path')
 module.exports = {
   db: 'mongodb://localhost/recommender',
+  cache: true,
+  numSimilar: 30,
   sentencesFile: path.join(__dirname, '../sentences-random.txt'),
   outputFile: path.join(__dirname, '../vectors.txt')
 }
