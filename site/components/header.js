dc523fddce2fe03a24b497f5a2bc272116662e03
freeman <the.freeman.lab@gmail.com>
2016-02-06 01:16:14 -0500

    update site

---
+++ site/components/header.js
@@ -0,0 +1,46 @@
+var css = require('dom-css')
+
+module.exports = function (container, opts) {
+
+  var colors = opts.colors
+  var fonts = opts.fonts
+  var ismobile = window.innerWidth < 600
+
+  var logo = container.appendChild(document.createElement('img'))
+  logo.width = '100'
+  logo.src = '../logo-transparent.svg'
+  logo.className = 'noselect'
+
+  var name = container.appendChild(document.createElement('img'))
+  name.width = '300'
+  name.src = '../name-transparent.svg'
+  name.className = 'noselect'
+
+  css(logo, {
+    position: 'absolute',
+    top: '15px',
+    left: '25px'
+  })
+
+  css(name, {
+    position: 'absolute',
+    top: '30px',
+    right: '20px'
+  })
+
+  if (ismobile) {
+    logo.width = '60'
+
+    css(logo, {
+      margin: '0 auto',
+      position: 'absolute',
+      right: '0px',
+      left: '0px'
+    })
+
+    css(name, {
+      display: 'none'
+    })
+  }
+
+}
---
+++ site/index.js
@@ -0,0 +1,38 @@
+var css = require('dom-css')
+var _ = require('lodash')
+
+var ismobile = window.innerWidth < 600
+
+var opts = {
+  colors: {
+    'green': 'rgb(117,215,66)',
+    'white': 'rgb(242,242,242)',
+    'brown': 'rgb(64,65,54)',
+    'darkbrown': 'rgb(14,15,4)'
+  },
+  fonts: {
+    'code': 'Hack',
+    'plain': 'Cooper Hewitt'
+  }
+}
+
+css(document.body, {
+  backgroundColor: opts.colors.green,
+  fontSize: window.innerWidth < 600 ? (window.innerWidth < 350 ? '9px' : '11px') : '15px',
+})
+
+var container = document.body.appendChild(document.createElement('div'))
+
+css(container, {
+  position: 'relative',
+  height: '100%'
+})
+
+var main = require('./components/main.js')(container, opts)
+var header = require('./components/header.js')(container, opts)
+var footer = require('./components/footer.js')(container, opts)
+
+
+
+
+
